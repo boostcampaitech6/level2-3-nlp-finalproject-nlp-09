0b4b1ebc381f99463fd513c448c6f81d2b309692
KSW <kimsw9703@kakao.com>
2024-03-19 15:04:07 +0900

    feat: remove mypage

--- streamlit/menu.py
+++ streamlit/menu.py
@@ -8,7 +8,6 @@ def authenticated_menu():
         st.rerun()
     st.sidebar.page_link("pages/diary.py", label="오늘의 일기")
     st.sidebar.page_link("pages/summary.py", label="요약")
-    st.sidebar.page_link("pages/mypage.py", label="마이페이지")
 
 
 def unauthenticated_menu():
