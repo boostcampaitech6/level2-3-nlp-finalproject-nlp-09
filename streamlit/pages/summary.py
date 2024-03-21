9dcbb882cff60abd183911096cb20658c7d6089d
KSW <kimsw9703@kakao.com>
2024-03-21 17:04:34 +0900

    feat: Remove word counter, Add emotion explanation

--- streamlit/pages/summary.py
+++ streamlit/pages/summary.py
@@ -13,7 +13,10 @@ from chatbot import *
 def main():
   menu_with_redirect()
   today = datetime.now().date()
-
+  emotion = json.loads(st.session_state['today_data']['emotion'].values[0])
+  emotion_title = [data[0] for data in emotion.values()]
+  emotion_value = [data[1] for data in emotion.values()]
+  
   st.title(f'{datetime.now().year}년 {datetime.now().month}월 {datetime.now().day}일 일기 요약')
   st.divider()
 
@@ -23,26 +26,15 @@ def main():
     st.header('오늘의 일기 요약')
     st.write(st.session_state['today_data']['summary'].values[0])
 
+    st.header('오늘의 감정 퍼센트')
+    st.write(f'오늘은 {st.session_state.id}님의 감정은 {emotion_title[0]} {emotion_value[0] * 100:.0f}%, {emotion_title[1]} {emotion_value[1] * 100:.0f}%, {emotion_title[1]} {emotion_value[1] * 100:.0f}%입니다.')
     col1, col2 = st.columns(2)
 
     with col1:
-      st.header('오늘의 감정 퍼센트')
-      emotion = json.loads(st.session_state['today_data']['emotion'].values[0])
-      emotion_title = [data[0] for data in emotion.values()]
-      emotion_value = [data[1] for data in emotion.values()]
       fig, ax = plt.subplots()
       ax.pie(emotion_value, labels=emotion_title, autopct='%1.1f%%')
-      st.pyplot(fig)
-      
-    with col2:
-      st.header('오늘의 단어 퍼센트')
-      word_list = json.loads(st.session_state['today_data']['word'].values[0])
-      word_list = Counter(word_list)
-      fig, ax = plt.subplots()
-      ax.bar(word_list.keys(), word_list.values())
 
       st.pyplot(fig)
-
 
 if __name__ == '__main__':
   main()
