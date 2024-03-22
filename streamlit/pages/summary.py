7d8a80d7de2ffd48ef697e9e017929215998dfbb
KSW <kimsw9703@kakao.com>
2024-03-22 18:08:10 +0900

    fix: Fix emotion json load error

--- streamlit/pages/summary.py
+++ streamlit/pages/summary.py
@@ -13,9 +13,6 @@ from chatbot import *
 def main():
   menu_with_redirect()
   today = datetime.now().date()
-  emotion = json.loads(st.session_state['today_data']['emotion'].values[0])
-  emotion_title = [data[0] for data in emotion.values()]
-  emotion_value = [data[1] for data in emotion.values()]
   
   st.title(f'{datetime.now().year}년 {datetime.now().month}월 {datetime.now().day}일 일기 요약')
   st.divider()
@@ -26,8 +23,12 @@ def main():
     st.header('오늘의 일기 요약')
     st.write(st.session_state['today_data']['summary'].values[0])
 
+    emotion = json.loads(st.session_state['today_data']['emotion'].values[0])
+    emotion_title = [data[0] for data in emotion.values()]
+    emotion_value = [data[1] for data in emotion.values()]
+
     st.header('오늘의 감정 퍼센트')
-    st.write(f'오늘은 {st.session_state.id}님의 감정은 {emotion_title[0]} {emotion_value[0] * 100:.0f}%, {emotion_title[1]} {emotion_value[1] * 100:.0f}%, {emotion_title[1]} {emotion_value[1] * 100:.0f}%입니다.')
+    st.write(f'오늘은 {st.session_state.id}님의 감정은 {emotion_title[0]} {emotion_value[0] * 100:.0f}%, {emotion_title[1]} {emotion_value[1] * 100:.0f}%, {emotion_title[2]} {emotion_value[2] * 100:.0f}%, 기타 {emotion_value[3] * 100:.0f}%입니다.')
     col1, col2 = st.columns(2)
 
     with col1:
