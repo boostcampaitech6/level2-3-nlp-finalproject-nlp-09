c9da120fdf50553861cf1798e109466f5457acb4
yunju <yunju0ju@yonsei.ac.kr>
2024-03-26 15:25:28 +0900

    fix: Remove summary code

--- models/chat/app/api.py
+++ models/chat/app/api.py
@@ -8,14 +8,9 @@ os.environ['HF_TOKEN'] = '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'
 
 router = APIRouter()
 
-summary_pipe = ChatPipe(
-    "CurtisJeon/OrionStarAI-Orion-14B-Chat-4bit", 
-    streamer=True
-)
-
 pipe = ChatPipe(
-    "CurtisJeon/OrionStarAI-Orion-14B-Chat-4bit", 
-    '/home/jhw/level2-3-nlp-finalproject-nlp-09/models/chat/trained/CurtisJeon-OrionStarAI-Orion-14B-Chat-4bit-20240326-014234', 
+    "EleutherAI/polyglot-ko-5.8b", 
+    'm2af/EleutherAI-polyglot-ko-5.8b-adapter', 
     streamer=True
 )
 
@@ -27,15 +22,6 @@ class ChatRequest(BaseModel):
 class ChatResponse(BaseModel):
     generation_id: int
     query: str
-    answer: str
-    
-class SummaryRequest(BaseModel):
-    generation_id: int
-    query: list # json list[dict]
-    
-class SummaryResponse(BaseModel):
-    generation_id: int
-    query: list
     answer: str
 
 
@@ -57,25 +43,4 @@ async def generate_text(req: ChatRequest) -> ChatResponse:
         generation_id=req.generation_id,
         query=req.query,
         answer=text
-    )
-
-@router.post("/summary/")
-async def summary_text(req: SummaryRequest) -> SummaryResponse:
-    try:
-        prompt = summary_pipe.summary_prompt(req.query)
-        result = summary_pipe.pipe(prompt)
-        text = summary_pipe.post_process(result)
-        
-    except RuntimeError:
-        raise HTTPException(status_code=500, detail="Model is not initialized")
-    except ValueError:
-        raise HTTPException(status_code=400, detail="Input is not valid")
-    except Exception as err:
-        print(err)
-        raise HTTPException(status_code=500, detail="Something went wrong")
-
-    return SummaryResponse(
-        generation_id=req.generation_id,
-        query=req.query,
-        answer=text.strip()
     )
