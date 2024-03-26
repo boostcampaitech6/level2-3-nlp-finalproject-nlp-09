38453bcda50e9900a530dcd6f9a1cd650f573ede
KSW <kimsw9703@kakao.com>
2024-03-26 14:09:57 +0900

    feat: 설명 페이지 추가

--- streamlit/main.py
+++ streamlit/main.py
@@ -57,6 +57,7 @@ def what_is_ed():
   st.write('하루의 일상을 마무리하면서 Dr.부덕이와 나눈 대화를 바탕으로 일기를 생성해주는 감정 일기 서비스')
   st.subheader('오늘의 일기를 쓰기 전에 부덕이가 할말이 있데요!')
   st.image('images/howto.png')
+  st.write('대화를 최소 4번 이상 주고받아야 Dr.부덕이가 일기를 요약할 수 있어!')
 
 def main():
   for key in st.session_state.keys():
